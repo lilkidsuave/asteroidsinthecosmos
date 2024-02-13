fb930c704ae9da5faf24fbd829334179951a48bd
lilkidsuave <66171643+lilkidsuave@users.noreply.github.com>
2024-02-13 08:53:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ for (const file of servapps) {
     servappsJSON.push(servapp)
   } catch (error) {
       if (error.message.includes('is not defined')) {
-      console.error(`Error loading description.json for ${file}: Skipping`);
+      console.error(`Error loading)define) description.json for ${file}: Skipping`);
       continue;
     } else {
       console.error(`Unknown Error`, error.message);
